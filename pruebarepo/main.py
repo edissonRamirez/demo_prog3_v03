7960c18006162cbc96a99156b7e904910f8d4a70
edissonRamirez <edisson.ramirez29529@ucaldas.edu.co>
2024-09-09 11:26:17 -0500

    Implementacion division

--- pruebarepo/main.py
+++ pruebarepo/main.py
@@ -1,5 +1,10 @@
 def resta(x,y):
     return x-y
 
+def division(x,y):
+    return x/y
+
 respuesta = resta(7-5)
 print("resultado es "+str(respuesta))
+resdivision = division(10,2)
+print(resdivision)
